8fa27b334745abd99a19df93b805e2508c573c39
EmanAU <241089@studetns.au.edu.pk>
2025-07-02 16:20:30 +0500

    commit resolved in new

--- oopproject.cpp
+++ oopproject.cpp
@@ -11,16 +11,9 @@ public:
 
 int main() {
 
-<<<<<<< HEAD
-
 	Feature f;
 	f.function();
 	cout << endl;
-
-=======
-	cout << "Hello world, this is the initial phase!" << endl;
-	cout << "This is the 2nd change in the main" << endl;
->>>>>>> main
 
 	return 0;
 }
